e70085af89135216ffb013def62a099db832f13d
ByEvidencias <izanosky30@gmail.com>
2024-08-24 00:04:35 +0200

    Update Quit.php

--- src/HolaJotos/ByEvidencias/events/Quit.php
+++ src/HolaJotos/ByEvidencias/events/Quit.php
@@ -4,25 +4,18 @@ namespace HolaJotos\ByEvidencias\events;
 
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerQuitEvent;
-use pocketmine\utils\TextFormat;
 use HolaJotos\ByEvidencias\Main;
 
 class Quit implements Listener {
 
-    private Main $plugin;
-
-    public function __construct(Main $plugin) {
-        $this->plugin = $plugin;
-    }
-
     public function onPlayerQuit(PlayerQuitEvent $event): void {
         $event->setQuitMessage("");
         $player = $event->getPlayer();
-        $config = $this->plugin->getConfig();
-        $message = $config->get("quit-message", "&c[-] &e{player} &cse ha salido del servidor!");
+        $plugin = Main::getInstance(); 
+        $message = $plugin->getConfig()->get("quit-message", "&c[-] &e{player} &cse ha salido del servidor!");
         $message = str_replace("{player}", $player->getName(), $message);
         $message = $this->translateColors($message);
-        $this->plugin->getServer()->broadcastMessage($message);
+        $plugin->getServer()->broadcastMessage($message);
         $event->setQuitMessage("");
     }
 
