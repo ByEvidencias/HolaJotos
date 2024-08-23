035665db50ad2a89189d062a3e27d12d2ebb03ea
ByEvidencias <izanosky30@gmail.com>
2024-08-24 00:03:46 +0200

    Update Join.php

--- src/HolaJotos/ByEvidencias/events/Join.php
+++ src/HolaJotos/ByEvidencias/events/Join.php
@@ -5,30 +5,24 @@ namespace HolaJotos\ByEvidencias\events;
 use pocketmine\player\Player;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\Listener;
-use pocketmine\plugin\PluginBase;
+use HolaJotos\ByEvidencias\Main;
 
 class Join implements Listener {
 
-    private PluginBase $plugin;
-
-    public function __construct(PluginBase $plugin) {
-        $this->plugin = $plugin;
-    }
-
     public function onPlayerJoin(PlayerJoinEvent $event): void { 
         $player = $event->getPlayer();
-        $config = $this->plugin->getConfig();
-        $message = $config->get("welcome-message", "&a[+] &e{player} &ase ha unido al servidor!");
+        $plugin = Main::getInstance();
+        $message = $plugin->getConfig()->get("welcome-message", "&a[+] &e{player} &ase ha unido al servidor!");
         $message = str_replace("{player}", $player->getName(), $message);
         $message = $this->translateColors($message);
-        $this->plugin->getServer()->broadcastMessage($message);
+        $plugin->getServer()->broadcastMessage($message);
         $this->sendPrivateMessage($player);
         $event->setJoinMessage("");
     }
 
     private function sendPrivateMessage(Player $player): void { 
-        $config = $this->plugin->getConfig();
-        $privateMessageLines = $config->get("private-message", []);
+        $plugin = Main::getInstance();
+        $privateMessageLines = $plugin->getConfig()->get("private-message", []);
         $privateMessage = implode("\n", $privateMessageLines);
         $privateMessage = str_replace("{player}", $player->getName(), $privateMessage);
         $privateMessage = $this->translateColors($privateMessage);
